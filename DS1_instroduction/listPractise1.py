bcef76893164eb511c0994416292f33b4f01c27c
Belmod <1228986180@qq.com>
2020-03-04 16:17:44 +0800

    十进制和各进制之间的转换

--- DS1_instroduction/listPractise1.py
+++ DS1_instroduction/listPractise1.py
@@ -1,4 +1,4 @@
-import time
+from timeit import Timer
 
 # 拼接
 def test1():
@@ -23,11 +23,20 @@ def test3():
 def test4():
     list1 = list(range(1000))
 
-if __name__ == '__main__':
-    test1()
-    test2()
-    test3()
-    test4()
 
-t1 = Timer('test1()','from__main__import test1')
-print(t1.timeit(number=1000))
+# t1 = Timer('test1()',"from __main__ import test1")
+# print('contact',t1.timeit(number=1000),'毫秒')
+# t2 = Timer("test2()", "from __main__ import test2")
+# print("append",t2.timeit(number=1000), "毫秒")
+# t3 = Timer("test3()", "from __main__ import test3")
+# print("comprehension ",t3.timeit(number=1000), "毫秒")
+# t4 = Timer("test4()", "from __main__ import test4")
+# print("list range ",t4.timeit(number=1000), "毫秒")
+
+
+x = range(2000000)
+pop_zero = Timer("x.pop(0)","from __main__ import x")
+print("pop_zero ",pop_zero.timeit(number=10000), "seconds")
+x = range(2000000)
+pop_end = Timer("x.pop()","from __main__ import x")
+print("pop_end ",pop_end.timeit(number=10000), "seconds")
---
+++ DS1_instroduction/stackPractise01.py
@@ -0,0 +1,111 @@
+# 栈抽象数据类型的底层实现采用什么？   List
+# 确定列表的哪一段是顶部，然后使用append和pop来实现操作
+# 假设列表的的尾部是栈的顶部元素，push
+
+
+from pythonds.basic.stack import Stack
+
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+#
+#     def isEmpty(self):
+#         return self.items == []
+#
+#     def push(self, item):
+#         self.items.append(item)
+#
+#     def pop(self):
+#         return self.items.pop()
+#
+#     def peek(self):
+#         return self.items[-1]
+#
+#     def size(self):
+#         return len(self.items)
+
+
+# s = Stack()
+# print(s.isEmpty())
+# s.push(4)
+# s.push('lala')
+
+# print(s.peek())
+
+# s.push(True)
+# print(s.size())
+
+# print(s.isEmpty())
+
+# s.push(10.9)
+# print(s.pop())
+# print(s.pop())
+# print(s.size())
+
+
+# ()匹配
+# def parChecker(symbolString):
+# # symbolString  假设"( () )"
+#
+#     s = Stack()
+#     flag = True
+#     index = 0
+#
+#     while index < len(symbolString) and flag:
+#         symbol = symbolString[index]
+#         if symbol == "(":
+#             s.push(symbol)
+#         else:
+#             if s.isEmpty():
+#                 flag = False
+#             else:
+#                 s.pop()
+#                 print(s)
+#         index = index + 1
+#
+#     if flag and s.isEmpty():
+#         return True
+#     else:
+#         return False
+#
+# print(parChecker('(())'))
+# print(parChecker('((())'))
+
+
+# {[()]}   (  [  )  ]
+# 每一个开始的符号被压入栈，等待匹配结果
+# 当出现结束符号的时候，必须检查栈顶部的开始符号是什么类型，如果两个符号不匹配，则字符串不匹配
+# 整个字符串处理完并且栈为空，则字符串匹配
+
+
+def parChecker(symbolString):
+    s = Stack()
+    flag = True
+    index = 0
+
+    while index < len(symbolString) and flag:
+        symbol = symbolString[index]
+
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                flag = False
+            else:
+                top = s.pop()
+                start = '([{'
+                end = ')]}'
+
+                if not start.index(top) == end.index(symbol):
+                    # index函数   根据括号里内容获取下标
+                    flag = False
+
+        index = index + 1
+
+    if flag and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parChecker('([{}])'))
+print(parChecker('([{}]]'))
